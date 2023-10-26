d5e835d95afe558b63ad6ce9ffe5dcbd543c83b9
Adrian Sokolowski <adrian.sokolowski00@gmail.com>
2023-10-26 21:21:56 +0200

    Navbar style and all pages.

--- src/assets/icons/TopRatedIcon.jsx
+++ src/assets/icons/TopRatedIcon.jsx
@@ -1,34 +1,12 @@
 const TopRatedIcon = () => {
   return (
     <svg
-      width="512px"
-      height="512px"
-      viewBox="0 0 512 24.00"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      stroke="#ffffff"
-      transform="rotate(0)"
+      width="36"
+      height="36"
+      viewBox="0 0 24 24"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0" />
-
-      <g
-        id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke="#CCCCCC"
-        stroke-width="0.336"
-      />
-
-      <g id="SVGRepo_iconCarrier">
-        {" "}
-        <path
-          d="M12 4.5L17 9.5M12 4.5L7 9.5M12 4.5L12 11M12 14.5C12 16.1667 11 19.5 7 19.5"
-          stroke="#ffffff"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />{" "}
-      </g>
+      <path d="m10 10.414 4 4 5.707-5.707L22 11V5h-6l2.293 2.293L14 11.586l-4-4-7.707 7.707 1.414 1.414z"></path>
     </svg>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,10 @@ import { NavLink, Link } from "react-router-dom";
 import { Searchbar } from "./Searchbar";
 import PopularIcon from "@icons/PopularIcon";
 import TopRatedIcon from "@icons/TopRatedIcon";
+import UpcomingIcon from "../assets/icons/UpcomingIcon";
+import NowPlayingIcon from "../assets/icons/NowPlayingIcon";
+import PopularTvSeriesIcon from "../assets/icons/PopularTvSeriesIcon";
+import TopRatedTvSeriesIcon from "../assets/icons/TopRatedTvSeriesIcon";
 
 const Navbar = () => {
   return (
@@ -9,7 +13,10 @@ const Navbar = () => {
       <div className="content-wrapper">
         <div className="navbar-top-container">
           <h1 className="title">
-            <Link to="/"> Moviebase <span> search your favorite movie </span></Link>
+            <Link to="/">
+              {" "}
+              Moviebase <span> search your favorite movie </span>
+            </Link>
           </h1>
           <Searchbar />
         </div>
@@ -23,27 +30,31 @@ const Navbar = () => {
             </li>
             <li>
               <NavLink to="/topRatedMovies" className="navlink">
-                <TopRatedIcon/>
+                <TopRatedIcon />
                 TopRated
               </NavLink>
             </li>
             <li>
               <NavLink to="/upcomingMovies" className="navlink">
+                <UpcomingIcon />
                 Upcoming
               </NavLink>
             </li>
             <li>
               <NavLink to="/nowPlayingMovies" className="navlink">
+                <NowPlayingIcon />
                 Now Playing
               </NavLink>
             </li>
             <li>
               <NavLink to="/popularSeries" className="navlink">
+                <PopularTvSeriesIcon />
                 Popular TV Series
               </NavLink>
             </li>
             <li>
               <NavLink to="/topRatedSeries" className="navlink">
+                <TopRatedTvSeriesIcon />
                 Top Rated TV Series
               </NavLink>
             </li>
